88281497244a6c4964b01816f4bd6535d73c175a
Stephanny Diana Alejandra Guevara Bocanegra <sdguevara85@misena.edu.co>
2022-03-19 23:30:20 -0500

    Se agregó una plantilla para poner los atributos de cada clase

---
+++ Clases/Message.php
@@ -0,0 +1,23 @@
+<?php
+class Message{
+    private $nombre;
+    function __construct($nombre){
+
+    }
+}
+//get
+function __get($propiedad)
+{
+    if(property_exists($this, $propiedad)){
+        return $this->$propiedad;
+    }
+}
+//set
+function __set($propiedad, $value)
+{
+    if(property_exists($this, $propiedad)){
+        return $this->$propiedad = $value;
+    }
+}
+    /*Este corchete cierra la clase, aquí solo deben ir métodos o sino dará error */
+?>
